cc954cf3bdd4f41230df80f481c295bdc87d5b1a
rata854 <nitiham2006@gmail.com>
2021-05-24 23:45:30 +0000

    [Add]Rubocop追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -12,7 +12,7 @@ class BlogsController < ApplicationController
   def new
     @blog = Blog.new
   end
-  
+
   def create
     blog = Blog.new(blog_path)
     blog.save
@@ -23,22 +23,22 @@ class BlogsController < ApplicationController
   def edit
     @blog = Blog.find(params[:id])
   end
-  
+
   def update
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog)
   end
-  
+
   def destroy
     blog = Blog.find(params[:id])
     blog.destroy
     redirect_to blogs_path
   end
-  
+
   private
+
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
   end
-  
 end
